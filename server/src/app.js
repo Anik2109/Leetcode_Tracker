0a639cf68039962db06f593c51a8b5c7ee764499
Anik2109 <anik.kaushesh@gmail.com>
2025-07-13 20:52:48 +0530

    --Backend
    Refactor CORS configuration to allow multiple origins dynamically

--- server/src/app.js
+++ server/src/app.js
@@ -4,12 +4,25 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://leetcode-tracker-woad.vercel.app"
+];
 
-
-app.use(cors({
-    origin: "http://localhost:5173", 
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like Postman) or from allowedOrigins
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("❌ Not allowed by CORS"));
+      }
+    },
     credentials: true
-}))
+  })
+);
+
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
